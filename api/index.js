97081b4d14951ec82747137859b27f9397e68d41
Manaram <tmsush123@gmail.com>
2024-08-20 20:12:14 +0530

    Create test Api

--- api/index.js
+++ api/index.js
@@ -15,3 +15,8 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server listning on port 3000')
 })
+
+
+import userRoutes from './routes/user.route.js'
+
+app.use("/api/user", userRoutes)
