0cbd4124b96951c36218fef8970742bfa05a6f2b
Manaram <tmsush123@gmail.com>
2024-08-20 15:08:55 +0530

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+},{timeseries: true}) //{timeseries: true} use to create some extra information to each user when creating //we no need to create manually them it will aotomatically do mongoose
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
