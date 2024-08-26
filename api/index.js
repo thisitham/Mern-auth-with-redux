f159d27504e6d68840f7d3b02c16d4011b1ea567
Manaram <tmsush123@gmail.com>
2024-08-26 13:38:55 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log('Server listning on port 3000')
